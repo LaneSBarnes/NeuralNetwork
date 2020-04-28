3d4ea2ed3a0843bf5bdecfae9ad434b871153027
LaneSBarnes <LaneSBarnes@gmail.com>
2020-04-27 23:51:24 -0600

    initialize arrays with no elements

--- NeuralNetwork/Neuron.cs
+++ NeuralNetwork/Neuron.cs
@@ -4,8 +4,8 @@
     {
         public double Activation { get; set; }
 
-        public double[] Weights { get; set; }
-        public double[] DCDW { get; set; }
+        public double[] Weights { get; set; } = new double[0];
+        public double[] DCDW { get; set; } = new double[0];
         public double Bias { get; set; }
         public double DCDB { get; set; }
     }
